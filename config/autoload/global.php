02f6382f0380d7a8d0c2eeb73a91bc159f9ecfba
Fabrizio Nicolosi <fabrizio@laylabs.it>
2015-12-14 23:38:51 +0100

    config files

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Global Configuration Override
  *
@@ -12,5 +13,19 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=ikimmobiliare_it_dotkernel;host=ufficio.laylabs.it',
+        'username' => 'SFN1_iki',
+        'password' => 'ikimmobiliare@123',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+            => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -17,7 +17,8 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+//                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Album\Controller\Album',
                         'action'     => 'index',
                     ),
                 ),
